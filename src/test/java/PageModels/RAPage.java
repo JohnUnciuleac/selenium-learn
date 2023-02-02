2857d3c1e10a79f4b9bcd31fb5da360ab4f3b44c
iunciuleac <iunciuleac@deloittece.com>
2023-02-02 15:21:13 +0200

    structural changes

---
+++ src/test/java/PageModels/RAPage.java
@@ -0,0 +1,60 @@
+package PageModels;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RAPage {
+
+    //Login page elements
+    @FindBy(xpath = "//input[@id='inputUsername']")
+    public WebElement inputUsername;
+
+    @FindBy(xpath = "//input[@name='inputPassword']")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[@class='submit signInBtn']")
+    public WebElement buttonSign;
+
+    @FindBy(xpath = "//p[@class='error']")
+    public WebElement errorText;
+
+    @FindBy(xpath = "//input[@id='chkboxOne']")
+    public WebElement checkboxRememberMe;
+
+    @FindBy(xpath = "//*[@id='chkboxTwo']")
+    public WebElement checkboxAgreeTAndC;
+
+    @FindBy(xpath = "//div[@class='forgot-pwd-container']/a")
+    public WebElement linkForgotYourPassword;
+
+
+    //Reset credentials page elements
+    @FindBy(xpath = "//input[@placeholder='Name']")
+    public WebElement inputNameResetCredentials;
+
+    @FindBy(xpath = "//input[@placeholder='Email']")
+    public WebElement inputEmailResetCredentials;
+
+    @FindBy(xpath = "//input[@placeholder='Phone Number']")
+    public WebElement inputPhoneNumberResetCredentials;
+
+    @FindBy(xpath = "//button[@class='reset-pwd-btn']")
+    public WebElement buttonResetLogin;
+
+    @FindBy(xpath = "//button[@class='go-to-login-btn']")
+    public WebElement buttonGoToLoginPage;
+
+    @FindBy(xpath = "//p[@class='infoMsg']")
+    public WebElement textInfoResetCredentials;
+
+    // Successful login page elements
+
+    @FindBy(xpath = "//div[@class='login-container']/p")
+    public WebElement textSuccessfulLogin;
+
+    @FindBy(xpath = "//button[@class='logout-btn']")
+    public WebElement buttonLogout;
+
+    @FindBy(xpath = "//div[@class='login-container']/h2")
+    public WebElement textUsernameSuccessfulLogin;
+}
--- src/test/java/com/example/demo/RATests.java
+++ src/test/java/com/example/demo/RATests.java
@@ -8,8 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import utils.BrowserHelper;
-import utils.RAPage;
+import PageModels.RAPage;
 
 import java.time.Duration;
 
@@ -23,6 +24,8 @@ public class RATests {
     void beforeAll() {
         driver = browserHelper.getChromeDriver();
         raPage = PageFactory.initElements(driver, RAPage.class);
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
     }
 
     @AfterAll
@@ -33,17 +36,52 @@ public class RATests {
     @Test
     void shouldNotBeAbleToLogin() {
 
-        driver.get("https://rahulshettyacademy.com/locatorspractice/");
-
         raPage.inputUsername.sendKeys("Selenium Test");
         raPage.inputPassword.sendKeys("Selenium");
 
         raPage.checkboxRememberMe.click();
-        raPage.checkboxAgreeTandC.click();
+        raPage.checkboxAgreeTAndC.click();
 
         raPage.buttonSign.click();
 
         System.out.println(new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(raPage.errorText)).getText());
+
+    }
+
+    @Test
+    void shouldBeAbleToResetCredentials() throws InterruptedException {
+
+        raPage.linkForgotYourPassword.click();
+
+        raPage.inputNameResetCredentials.sendKeys("Reset Credentials Test");
+        raPage.inputEmailResetCredentials.sendKeys("rahulshettyacademy");
+        raPage.inputPhoneNumberResetCredentials.sendKeys("8593762834");
+
+        Thread.sleep(2000);
+
+        raPage.buttonResetLogin.click();
+
+        System.out.println(raPage.textInfoResetCredentials.getText());
+
+    }
+
+    @Test
+    void shouldBeAbleToLogin() throws InterruptedException {
+
+        raPage.inputUsername.sendKeys("Selenium Test");
+        raPage.inputPassword.sendKeys("rahulshettyacademy");
+
+        raPage.checkboxRememberMe.click();
+        raPage.checkboxAgreeTAndC.click();
+
+        raPage.buttonSign.click();
+
+        Thread.sleep(3000);
+
+        Assert.assertEquals(raPage.textSuccessfulLogin.getText(),"You are successfully logged in.");
+        Assert.assertEquals(raPage.textUsernameSuccessfulLogin.getText(), "Hello Selenium Test,");
+
+        raPage.buttonLogout.click();
 
     }
 
